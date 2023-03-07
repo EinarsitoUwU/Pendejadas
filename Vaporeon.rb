4649319e3274e281aa2bbd9755f9d9cdc34033df
Einar Cabrera <einarjahazielcabreraamador@hotmail.com>
2023-03-07 00:58:40 -0600

    First Program

---
+++ Vaporeon.rb
@@ -0,0 +1,12 @@
+name = gets.chomp
+puts("Oye, ¿sabías que en términos de reproduccion de Pokémon humanos masculinos y femeninos, #{name} es el Pokémon más compatible para humanos.
+
+
+No solo están en el grupo de huevos de campo, que está compuesto principalmente por mamíferos, los #{name} miden un promedio de 3 03 de altura y pesan 63.9 libras.
+
+Esto significa que son lo suficientemente grandes como para poder soportar el pene humano, y con sus impresionantes estadísticas base para HP y acceso a Armadura Acida, incluso puedes ser rudo con uno.
+
+Debido a su biología principalmente basada en agua, no tengo ninguna duda de que un #{name} excitado estaría increíblemente mojado, tan mojado que fácilmente podrías tener sexo con uno. durante horas sin secarse.
+
+También pueden aprender los movimientos Atraer, Ojos de muñeca, Cautivar, Encanto y Látigo de cola, además de no tener pelaje para ocultar los pezones, por lo que sería increíblemente fácil para uno ponerte cachondo.Sabias que  #{name} " )
+
